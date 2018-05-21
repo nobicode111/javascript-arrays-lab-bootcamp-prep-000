dbf33c9fd3163f5e8114c37addf3952461760aed
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-21 19:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
