d42715d972b5ddd09b39db1cde64299100decff5
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-21 19:41:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function prependKitten(name) {
   return newkittens
 }
 function removeLastKitten() {
-  kittens.slice(kittens.length - 1)
+  kittens.slice(0, kittens.length - 1)
   return kittens
 }
